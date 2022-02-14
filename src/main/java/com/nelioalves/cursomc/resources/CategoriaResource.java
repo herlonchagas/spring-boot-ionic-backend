604ac359089a542e07078cfdcf51cedd95cf605d
herlonchagas <herlonchagas.c@gmail.com>
2022-02-14 20:36:06 -0300

    Paginação com paramentros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -2,23 +2,22 @@ package com.nelioalves.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Objects;
 
-import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
-import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -67,6 +66,17 @@ public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<CategoriaDTO> list = service.findAllCategoria();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "2") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
+		Page<CategoriaDTO> list = service.findPage(page, linesPerPage, orderBy, direction);
 		
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -9,6 +9,9 @@ import javax.validation.ConstraintViolationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -65,5 +68,11 @@ public class CategoriaService {
 				.collect(Collectors.toList());
 	
 		return listDTO;
+	}
+	
+	public Page<CategoriaDTO> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest).map(categora -> CategoriaMapper.INSTANCE.categoriaToCategoriaDTO(categora));
 	}
 }
